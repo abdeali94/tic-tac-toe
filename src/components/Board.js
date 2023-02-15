87b691d7a210e2fc83238c68d00aa0df22c32f32
Abde <ab.abdeali.abuwala@gmail.com>
2023-02-15 17:59:30 +0530

    initial commit

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import {Box} from './Box';
+import './Board.css';
+
+export const Board = ({board, onClick}) => {
+    return (
+        <div className="board">
+            {board.map((value, index) => <Box value={value} onClick={() => value === null && onClick(index)} />)}
+        </div>
+    );
+}
